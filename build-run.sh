aca565bd96b0ec2ec732ffdc8a95ed88d31c6951
matentzn <nicolas.matentzoglu@gmail.com>
2019-08-18 10:06:06 +0100

    Run should fail if single error occurs

---
+++ build-run.sh
@@ -0,0 +1,2 @@
+docker build . -t matentzn/vfb-pipeline-kb2kb
+docker run --env=KBpassword=neo4j/neo --env=KBserver=http://host.docker.internal:7474 matentzn/vfb-pipeline-kb2kb
--- process.sh
+++ process.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "Transform old KB to new schema"
 
 echo 'START' >> ${WORKSPACE}/tick.out
