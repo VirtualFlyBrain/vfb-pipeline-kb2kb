dde17e30bc79f421e114d9bf9dcde4aa106b07c2
matentzn <nicolas.matentzoglu@gmail.com>
2019-08-21 10:18:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+#SERVER=http://host.docker.internal:7474
+SERVER=http://kb.p2.virtualflybrain.org
+
 docker pull matentzn/vfb-pipeline-kb2kb
-docker run --env=KBpassword=neo4j/neo --env=KBserver=http://host.docker.internal:7474 matentzn/vfb-pipeline-kb2kb
+docker run --env=KBpassword=neo4j/neo --env=KBserver=$SERVER matentzn/vfb-pipeline-kb2kb
